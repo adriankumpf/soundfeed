6892f68232b7ada2fdd888d5c2c4927b213d4f3b
Adrian Kumpf <adrian.kumpf@posteo.de>
2017-04-21 18:31:43 +0200

    Initial commit

---
+++ lib/soundcloud_rss.ex
@@ -0,0 +1,18 @@
+defmodule SoundcloudRss do
+  @moduledoc """
+  Documentation for SoundcloudRss.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> SoundcloudRss.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/soundcloud_rss_test.exs
@@ -0,0 +1,8 @@
+defmodule SoundcloudRssTest do
+  use ExUnit.Case
+  doctest SoundcloudRss
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
